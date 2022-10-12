20ce0e25d544d8b528be89ee34cda787a24b222e
Renata <renatalcaetana@gmail.com>
2022-10-12 08:39:27 -0300

    [FEAT] Producer and consumer topics

---
+++ projeto-atual/ecommerce/ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -0,0 +1,32 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class EmailService {
+    public static void main(String[] args) {
+        var emailService = new EmailService();
+        try(var service = new KafkaService(
+                EmailService.class.getSimpleName(),
+                "ECOMMERCE2_SEND_EMAIL",
+                emailService::parse, Order.class)){
+        service.run();
+    }}
+
+    private void parse(ConsumerRecord<String, String> record){
+        System.out.println("-----------------------------------------------");
+        System.out.println("Send email, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Email sent");
+    }
+
+
+
+}
---
+++ projeto-atual/ecommerce/ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -0,0 +1,33 @@
+package br.com.alura.ecommerce;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<Email>()) {
+                for (var i = 0; i < 10; i++) {
+                    var key = UUID.randomUUID().toString();
+
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
+
+
+                    var value = key + ",124444,6777777";
+                    orderDispatcher.send("ECOMMERCE2_NEW_ORDER", userId, order);
+
+                    var email = new Email("Teste", "Thank you for your order! We are processing your order!");
+
+                    emailDispatcher.send("ECOMMERCE2_SEND_EMAIL", key, email);
+                }
+            }
+        }
+
+    }
+
+}
